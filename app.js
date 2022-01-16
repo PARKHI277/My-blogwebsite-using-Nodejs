5ab2109d523ac5089b6fc02b035e1c199d68c504
Parkhi <parkhigarg277@gmail.com>
2022-01-16 23:09:54 +0530

    challenge 12

--- app.js
+++ app.js
@@ -7,13 +7,17 @@ app.set('view engine','ejs');
 app.use(bodyparser.urlencoded({extended:true}));
 app.use(express.static("public"));
 
+let posts = [];
+
 const homec = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi ullam reiciendis asperiores, dolore est nam in debitis inventore eaque explicabo quod saepe maxime quaerat voluptas ab pariatur sint. Pariatur, nam.";
 const aboutc = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi ullam reiciendis asperiores, dolore est nam in debitis inventore eaque explicabo quod saepe maxime quaerat voluptas ab pariatur sint. Pariatur, nam.";
 const contactc = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi ullam reiciendis asperiores, dolore est nam in debitis inventore eaque explicabo quod saepe maxime quaerat voluptas ab pariatur sint. Pariatur, nam."; 
 
 app.get("/",function(req,res)
 {
-     res.render("home",{deliver:homec});
+     res.render("home",{deliver:homec,
+    posts:posts});
+  
 });
 
 app.get("/about",function(req,res)
@@ -30,6 +34,8 @@ app.get("/compose",function(req,res)
     res.render("compose");
 })
 
+
+
 app.post("/compose",function(req,res)
 {
     
@@ -37,6 +43,10 @@ app.post("/compose",function(req,res)
         title: req.body.Createpost,
         content: req.body.main
     };
+
+    posts.push(create);
+
+    res.redirect("/");
     
 })
 
