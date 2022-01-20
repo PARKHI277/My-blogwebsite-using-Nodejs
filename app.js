23b40b0ab431a848dc5afa0477ea08c14caea5bc
Parkhi <parkhigarg277@gmail.com>
2022-01-20 23:49:45 +0530

    added gitignore

--- app.js
+++ app.js
@@ -76,7 +76,13 @@ app.get("/contact", function(req, res){
   res.render("contact", {contactContent: contactc});
 });
 
+let port = process.env.PORT;
+if(port == null || port == "")
+{
+  port = 3000;
+}
+app.listen(port);
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(port, function() {
+  console.log("Server has started succesfully");
 });
