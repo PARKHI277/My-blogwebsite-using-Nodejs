6de0b4388baa3a88b7297bdf39ed4e8abe410603
Parkhi Garg <76432727+PARKHI277@users.noreply.github.com>
2022-10-09 13:57:50 +0530

    Revert "added backend code and modified code Structure"

--- models/postModels.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
-
-const postSchema = new Schema({
-  title: String,
-  content: String,
-})
-
-module.exports = mongoose.model('Post', postSchema)
